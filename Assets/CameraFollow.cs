d9fba7f329a4305052c3e4dd2495077cad793a00
Shaun <shaunkorba@cmail.carleton.ca>
2019-11-06 13:37:09 -0500

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 999999999f;
+
+    public Vector3 offset;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = target.position + offset * 0.6f + new Vector3(0,6,-3);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed* (Time.unscaledDeltaTime*41));
+
+
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Objects/Player/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody theRB;
+
+    public float slowDownFactor = 0.2f;
+    public float slowDownLength = 1f;
+
+
+
+    void slowTime()
+    {
+        //Time.timeScale = slowDownFactor;
+        Time.timeScale -= (3f / slowDownLength) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, slowDownFactor, 1f);
+
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale += (1f / slowDownLength) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+
+
+        if (Input.GetMouseButton(0))
+        {
+            slowTime();
+            Debug.Log("Yep");
+        }
+
+
+        theRB.velocity = new Vector3(Input.GetAxis("Horizontal")*moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical")* moveSpeed);
+
+
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float midPoint = (transform.position - Camera.main.transform.position).magnitude; //dist between this and camera
+
+        Vector3 lookPoint = mouseRay.origin + mouseRay.direction * midPoint;
+
+        lookPoint.y = transform.position.y;
+
+        transform.LookAt(lookPoint);
+
+
+
+
+        //Ortho
+        //transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+    }
+}
