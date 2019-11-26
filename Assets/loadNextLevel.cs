6487bde579ef14be7073ab13157107d3a47112bf
Sven Whitesell <sven.whitesell@gmail.com>
2019-11-26 17:40:49 -0500

    made 2 levels and a temp 3rd level

---
+++ Assets/loadNextLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadNextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
+   
