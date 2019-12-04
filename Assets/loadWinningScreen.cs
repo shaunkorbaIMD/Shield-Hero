2e080e1d4f5638a9a101731b8cc707b6c02e4884
Sven Whitesell <sven.whitesell@gmail.com>
2019-12-04 17:39:27 -0500

    winning screen, fixed up levels

---
+++ Assets/loadWinningScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadWinningScreen : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("winningScreen");
+        }
+    }
+}
+
+
